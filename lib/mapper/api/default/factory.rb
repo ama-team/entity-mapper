d19b2448a122633e2b36564228aa74ea580f4109
Etki <etki@etki.me>
2017-07-30 22:07:59 +0300

    Added support for filling freshly-created entity with default attribute values

--- lib/mapper/api/default/factory.rb
+++ lib/mapper/api/default/factory.rb
@@ -2,6 +2,7 @@
 
 require_relative '../factory'
 require_relative '../../mixin/errors'
+require_relative '../../path/segment'
 
 module AMA
   module Entity
@@ -17,14 +18,27 @@ module AMA
             # @param [AMA::Entity::Mapper::Type] type
             # @param [Object] _data
             # @param [AMA::Entity::Mapper::Context] context
-            def create(type, _data = nil, context = nil)
-              type.type.new
+            def create(type, _data, context)
+              create_internal(type)
             rescue StandardError => e
               message = "Failed to instantiate #{type} directly from class"
               if e.is_a?(ArgumentError)
                 message += '. Does it have parameterless #initialize() method?'
               end
               mapping_error(message, parent: e, context: context)
+            end
+
+            private
+
+            def create_internal(type)
+              entity = type.type.new
+              type.attributes.values.each do |attribute|
+                next if attribute.default.nil? || attribute.virtual
+                segment = Path::Segment.attribute(attribute.name)
+                value = attribute.default
+                type.injector.inject(entity, type, attribute, value, segment)
+              end
+              entity
             end
           end
         end
